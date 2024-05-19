e42b82438487fb29f10ab7127ad8799415440588
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-05-20 00:01:03 +0500

    Add JavaScript if, else if staement with variable ++.

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ function swapImage(event, type){
 
 
 // // Second Type
-// var count = 0;
-// function swapImage(){
-//     if (count == 0){
-//         event.target.src = "https:st.depositphotos.com/28789600/51824/i/450/depositphotos_518242018-stock-photo-golden-colors-most-beautiful-sunset.jpg"
-//     count = 1;
-//     }else{
-//         event.target.src = "https://st.depositphotos.com/2001755/3622/i/450/depositphotos_36220949-stock-photo-beautiful-landscape.jpg"
-//         count = 0;
-//     }
-// }
+var count = 0;
+function swapImage(){
+    if (count == 0){
+        event.target.src = "https:st.depositphotos.com/28789600/51824/i/450/depositphotos_518242018-stock-photo-golden-colors-most-beautiful-sunset.jpg"
+    count = 1;
+    }else{
+        event.target.src = "https://st.depositphotos.com/2001755/3622/i/450/depositphotos_36220949-stock-photo-beautiful-landscape.jpg"
+        count = 0;
+    }
+}
 
 // // Third Type
 // function swapImage(){
